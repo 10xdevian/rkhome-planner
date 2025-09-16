637a1e2965428f5fb68a22b5324e88f8f2a8768d
nitish <mandalnitish570@gmail.com>
2025-09-16 19:22:55 +0530

    Nav bar added and diasy ui is installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
 import Image from "next/image";
-
+import Nav from "../components/Nav";
 export default function Home() {
-<<<<<<< HEAD
-  return (
-   <div>
-    WElcome to the Rk-Home Planner
-   </div>
-  );
-=======
- return(
+return (
   <div>
-    Welcome to Rk-Home Planner!
-    <Image
-      className="dark:invert"
-      src="/next.svg"
-      alt="Next.js logo"
-      width={180}
-      height={38}
-      priority
-    />
+    <Nav />
   </div>
- );
->>>>>>> main
+)
 }
