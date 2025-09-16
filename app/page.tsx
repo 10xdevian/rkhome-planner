8747011e044b077aa664977a7484ab9730d3f373
nitish <mandalnitish570@gmail.com>
2025-09-16 12:24:56 +0530

    Add one btn

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
  return(
   <div>
     Welcome to Rk-Home Planner!
+    <Image
+      className="dark:invert"
+      src="/next.svg"
+      alt="Next.js logo"
+      width={180}
+      height={38}
+      priority
+    />
   </div>
  );
 }
